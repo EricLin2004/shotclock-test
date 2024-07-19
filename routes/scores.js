bc7b0c718f4e3f54864b2e09ecae99e23ce9448c
EricLin2004 <eric@axiomzen.co>
2024-07-18 17:00:10 -0700

    add totalGamesPlayed counter'

--- routes/scores.js
+++ routes/scores.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 let scores = [];
+let totalGamesPlayed = 0;
 
 // Post new score
 router.post('/', (req, res, next) => {
@@ -10,12 +11,17 @@ router.post('/', (req, res, next) => {
         return b.score - a.score;
     });
 
+    totalGamesPlayed++;
+
     res.sendStatus(200);
 });
 
 router.get('/', (req, res, next) => {
     console.log('scores: ', scores);
-    res.send({ scores: scores.slice(0, 20) });
+    res.send({ 
+        scores: scores.slice(0, 20),
+        totalGamesPlayed
+    });
 });
 
 module.exports = router;
